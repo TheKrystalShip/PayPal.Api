72ba6b7935f09aee1b1a3cc60fdb09b7b358e7d2
Cristian Moraru <cristian.moraru@live.com>
2018-07-29 14:12:03 +0200

    Initial Setup

---
+++ PayPal.Api/PaymentService.cs
@@ -0,0 +1,20 @@
+﻿using Braintree;
+
+using TheKrystalShip.PayPal.Api.Properties;
+
+namespace PayPal.Api
+{
+    public class PaymentService
+    {
+        public PaymentService()
+        {
+            BraintreeGateway gateway = new BraintreeGateway
+            {
+                Environment = Environment.SANDBOX,
+                MerchantId = Settings.Instance["Braintree:MerchantId"],
+                PublicKey = Settings.Instance["Braintree:PublicKey"],
+                PrivateKey = Settings.Instance["Braintree:PrivateKey"]
+            };
+        }
+    }
+}
---
+++ PayPal.Api/Properties/Settings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using System.IO;
+
+namespace TheKrystalShip.PayPal.Api.Properties
+{
+    public class Settings
+    {
+        private static IConfiguration _config;
+
+        public static IConfiguration Instance
+        {
+            get
+            {
+                if (_config != null)
+                    return _config;
+
+                return _config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile(Path.Combine("Properties", "settings.json"))
+                    .Build();
+            }
+        }
+    }
+}
