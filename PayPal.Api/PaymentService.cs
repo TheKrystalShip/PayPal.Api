7250e33478ef60f11111c5c89ed5518c598ff59c
Hamza Saddouki <hamza_saddouki@outlook.es>
2018-08-15 14:37:49 +0100

    Modified Payment Service To Create ClientTokens and CreatePayments

--- PayPal.Api/PaymentService.cs
+++ PayPal.Api/PaymentService.cs
@@ -1,20 +1,60 @@
 ﻿using Braintree;
-
 using TheKrystalShip.PayPal.Api.Properties;
 
 namespace TheKrystalShip.PayPal.Api
 {
     public class PaymentService
     {
+        BraintreeGateway Gateway;
+
         public PaymentService()
         {
-            BraintreeGateway gateway = new BraintreeGateway
+            Gateway = new BraintreeGateway
             {
                 Environment = Environment.SANDBOX,
                 MerchantId = Settings.Instance["Braintree:MerchantId"],
                 PublicKey = Settings.Instance["Braintree:PublicKey"],
                 PrivateKey = Settings.Instance["Braintree:PrivateKey"]
             };
+        }
+
+        public string GetGateway()
+        {
+            var clientToken = Gateway.ClientToken.Generate();
+            return clientToken;
+        }
+
+        public string CreatePayment(decimal Amount, string PaymentMethodNonce)
+        {
+            var request = new TransactionRequest
+            {
+                Amount = Amount,
+                PaymentMethodNonce = PaymentMethodNonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = Gateway.Transaction.Sale(request);
+            if (result.IsSuccess())
+            {
+                Transaction transaction = result.Target;
+                return transaction.Id;
+            }
+            else if (result.Transaction != null)
+            {
+                return result.Transaction.Id;
+            }
+            else
+            {
+                string errorMessages = "";
+                foreach (ValidationError error in result.Errors.DeepAll())
+                {
+                    errorMessages += "Error: " + (int)error.Code + " - " + error.Message + "\n";
+                }
+                return errorMessages;
+            }
         }
     }
 }
